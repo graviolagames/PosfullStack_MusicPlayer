4f36fd2a2803d376fa482fcb2218e9794ba76fe5
Larissa170 <larissa.m.rodrigues1702@gmail.com>
2023-10-11 21:36:29 -0300

    Busca da música
    Signed-off-by: Larissa170 <larissa.m.rodrigues1702@gmail.com>

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const musicController = require('./controllers/musicController');
+const musicController = require("./controllers/musicController");
 
 // Configuração do template engine EJS
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 //Permite utilizar rotas de arquivos estáticos
-app.use(express.static(__dirname + '/Music'));
+app.use("/Music", express.static(__dirname + "/Music"));
 
 // Middleware para processar o corpo das requisições
 app.use(express.urlencoded({ extended: true }));
 
 // Rota principal
-app.use('/', musicController);
+app.use("/", musicController);
 
 // Iniciar o servidor
 app.listen(3000, () => {
-  console.log('Servidor iniciado na porta 3000');
+  console.log("Servidor iniciado na porta 3000");
 });
--- controllers/musicController.js
+++ controllers/musicController.js
@@ -3,22 +3,27 @@ const router = express.Router();
 const Music = require("../models/music");
 
 // Simulando um banco de dados
-const Musica1 = new Music(
-  1,
-  "Get Ready for this",
-  "http://localhost:3000/Music/GetReadyForThis.mp3"
-);
-const Musica2 = new Music(2, "IRan", "http://localhost:3000/Music/IRan.mp3");
-const musics = [Musica1, Musica2];
+const musics = [
+  new Music(
+    1,
+    "Get Ready for this",
+    "GetReadyForThis.mp3"
+  ),
+  new Music(
+    2, 
+    "IRan", 
+    "IRan.mp3"
+  )
+];
 
-// Rota para exibir a lista de musicas utilizando o Template Engine
+// Rota para exibir a página do player
 router.get("/", (req, res) => {
-  res.render("index_Music", { musics });
+  res.render("index");
 });
 
-// Rota para exibir a lista de musicas utilizando o JSON
+// API - Rota para recuperar as músicas
 router.get("/musics", (req, res) => {
-  res.json(musics);
+  res.status(200).json(musics);
 });
 
 module.exports = router;
